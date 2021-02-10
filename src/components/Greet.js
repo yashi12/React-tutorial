16211bc9d548f96490f1dd6f6366ba42174f415b
yashi12 <yashiagarwal1812@gmail.com>
2021-02-11 00:44:40 +0530

    Update way to access props

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Greet=(props) =>{
+    const {name,age} = props; // extracting name and age from props
     return (
         <div>
-            <h1>hello {props.name}</h1>
-            <p>{props.age}</p>
+            <h1>hello {name}</h1>
+            <p>{age}</p>
             {props.children}
         </div>
     );
